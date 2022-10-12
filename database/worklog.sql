ed1bada762409a6a389e05e93222359caaf593c3
Ravi Chandra Sekhar Meda <ravi.meda@xius-bcgi.ltd>
2022-10-12 09:52:56 +0530

    Updated-Added user

--- database/worklog.sql
+++ database/worklog.sql
@@ -1,1 +1,4 @@
-create table story (id int auto_increment primary key, story_title varchar(1024), story_description TEXT, theme_id int, epic_id int, story_type int);  
+
+CREATE TABLE IF NOT EXISTS story (id int auto_increment primary key, story_title varchar(1024), story_description TEXT, theme_id int, epic_id int, story_type int, story_points int);
+CREATE TABLE IF NOT EXISTS tasks (id int auto_increment primary key, story_id int, task_description TEXT, estimated_hours int, actual_hours int);  
+CREATE TABLE IF NOT EXISTS members(id int auto_increment primary key, company_id varchar(20), name varchar(128));
